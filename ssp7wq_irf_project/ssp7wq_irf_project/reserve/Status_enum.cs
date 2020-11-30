1ec3d1079bc61fcd739efe2d51599026688be619
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-11-30 23:34:50 +0100

    enum

---
+++ ssp7wq_irf_project/ssp7wq_irf_project/reserve/Status_enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssp7wq_irf_project.reserve
+{
+    public enum Status_enum
+    {
+        Szabad=1,
+        Kivalasztva=2,
+        Foglalt=3
+    }
+}

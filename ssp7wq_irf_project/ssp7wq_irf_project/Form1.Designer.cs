229bcf3a44cc4f4a9c09cdd1b7a97bdc02542b49
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-12-03 21:37:55 +0100

    szures_done

--- ssp7wq_irf_project/ssp7wq_irf_project/Form1.Designer.cs
+++ ssp7wq_irf_project/ssp7wq_irf_project/Form1.Designer.cs
@@ -30,16 +30,20 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 42);
+            this.listBox1.Location = new System.Drawing.Point(12, 74);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(122, 100);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // comboBox1
             // 
@@ -47,22 +51,54 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Film címe",
             "Dátum"});
-            this.comboBox1.Location = new System.Drawing.Point(13, 12);
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(122, 24);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(140, 10);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 164);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(266, 10);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 164);
+            this.listBox3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +106,9 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ssp7wq_irf_project/ssp7wq_irf_project/Form1.cs
+++ ssp7wq_irf_project/ssp7wq_irf_project/Form1.cs
@@ -16,14 +16,16 @@ namespace ssp7wq_irf_project
     {
         Mozi_jegy_musor_kezelesEntities context = new Mozi_jegy_musor_kezelesEntities();
 
-        BindingSource FilmBindingSource = new BindingSource();
-        BindingSource MusorBindingSource = new BindingSource();
+        //BindingSource FilmBindingSource = new BindingSource();
+        //BindingSource MusorBindingSource = new BindingSource();
+        //BindingSource IdopontBindingSource = new BindingSource();
         public Form1()
         {
             InitializeComponent();
 
             context.Film.Load();
             context.Musor.Load();
+
 
             LoadMenu();
 
@@ -33,24 +35,49 @@ namespace ssp7wq_irf_project
 
         private void LoadMenu()
         {
+            ListBox_refresh();
             var szuro = comboBox1.SelectedItem;
             switch (szuro)
             {
                 case "Film címe":
+                    textBox1.Enabled = true;
+                    textBox1.Clear();
+                    //listBox2.DataSource = MusorBindingSource;
+                    //listBox3.DataSource = IdopontBindingSource;
+
                     var cim = (from x in context.Film
-                               select x.Cím);
-                    FilmBindingSource.DataSource = cim.ToList();
-                    listBox1.DataSource = FilmBindingSource;
+                               where x.Cím.StartsWith(textBox1.Text)
+                               select x);
+                    listBox1.DisplayMember = "Cím";
+                    listBox1.DataSource = cim.ToList();
+
+                    /*
+                    var musor = (from x in context.Musor
+                                 select x.Datum);
+                    MusorBindingSource.DataSource = musor.ToList();
+
+                    var idopont = (from x in context.Musor
+                                 select x.Idopont);
+                    IdopontBindingSource.DataSource = musor.ToList();
+                    */
                     break;
                 case "Dátum":
+                    textBox1.Enabled = false;
                     var datum = (from x in context.Musor
-                                 select x.Datum);
-                    MusorBindingSource.DataSource = datum.Distinct().ToList();
-                    listBox1.DataSource = MusorBindingSource;
+                                 select x);
+                    listBox1.DisplayMember = "Datum";
+                    listBox1.DataSource = datum.Distinct().ToList();
                     break;
                 default:
                     break;
             }
+        }
+
+        private void ListBox_refresh()
+        {
+            listBox1.Refresh();
+            listBox2.Refresh();
+            listBox3.Refresh();
         }
 
         private void LoadPanel()
@@ -65,6 +92,85 @@ namespace ssp7wq_irf_project
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadMenu();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox_refresh();
+            var szuro = comboBox1.SelectedItem;
+            switch (szuro)
+            {
+                case "Film címe":
+                    textBox1.Enabled = true;
+                    textBox1.Clear();
+                    //listBox3.DataSource = IdopontBindingSource;
+
+                    var film = ((Film)listBox1.SelectedItem).Id_Film;
+                    var musor = (from x in context.Musor
+                                 where x.Film_Id==film
+                                 select x);
+                    listBox2.DisplayMember = "Datum";
+                    listBox2.DataSource = musor.ToList();
+                    /*
+                    var idopont = (from x in context.Musor
+                                   select x.Idopont);
+                    IdopontBindingSource.DataSource = musor.ToList();
+                    */
+                    break;
+                case "Dátum":
+                    textBox1.Enabled = false;
+                    var musor_aznapi = ((Musor)listBox1.SelectedItem);
+                    var cim =   (from x in context.Musor
+                                 where x.Datum==musor_aznapi.Datum
+                                 select x.Film);
+                    listBox2.DisplayMember = "Cím";
+                    listBox2.DataSource = cim.Distinct().ToList();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox_refresh();
+            var szuro = comboBox1.SelectedItem;
+            switch (szuro)
+            {
+                case "Film címe":
+                    textBox1.Enabled = true;
+                    textBox1.Clear();
+                    var film = ((Film)listBox1.SelectedItem).Id_Film;
+                    var musor = ((Musor)listBox2.SelectedItem).Datum;
+                    var idop = (from x in context.Musor
+                                 where x.Film_Id == film && x.Datum==musor
+                                 select x);
+                    listBox3.DisplayMember = "Idopont";
+                    listBox3.DataSource = idop.ToList();
+                    /*
+                    var idopont = (from x in context.Musor
+                                   select x.Idopont);
+                    IdopontBindingSource.DataSource = musor.ToList();
+                    */
+                    break;
+                case "Dátum":
+                    textBox1.Enabled = false;
+                    var filmcim = ((Film)listBox2.SelectedItem).Id_Film;
+                    var datum = ((Musor)listBox1.SelectedItem).Datum;
+                    var idopont = (from x in context.Musor
+                                   where x.Datum==datum && x.Film_Id==filmcim
+                                 select x);
+                    listBox3.DisplayMember = "Idopont";
+                    listBox3.DataSource = idopont.ToList();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
             LoadMenu();
         }
